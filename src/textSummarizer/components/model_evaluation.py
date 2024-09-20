1f1af8487e1e4884760abac52438158c3067b9ec
deepakpw234 <deepakpw234@gmail.com>
2024-09-20 14:02:21 +0530

    CICD Deployment

--- src/textSummarizer/components/model_evaluation.py
+++ src/textSummarizer/components/model_evaluation.py
@@ -66,7 +66,7 @@ class ModelEvaluation:
         rouge_metric = load_metric('rouge',trust_remote_code=True)
 
         score = self.calculate_metric_on_test_ds(
-                dataset_samsum_pt['test'][0:10], rouge_metric, model_pegasus, tokenizer, 
+                dataset_samsum_pt['test'], rouge_metric, model_pegasus, tokenizer, 
                 batch_size = 2, device=device,column_text = 'dialogue', 
                 column_summary= 'summary')
 
